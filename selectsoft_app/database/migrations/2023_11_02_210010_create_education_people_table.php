20a397785b30222e063440d89b74772516417699
Camilo <camilo222andresace@gmail.com>
2023-11-02 16:48:48 -0500

    study levels and ecucation people tables created

--- selectsoft_app/database/migrations/2023_11_02_210010_create_education_people_table.php
+++ selectsoft_app/database/migrations/2023_11_02_210010_create_education_people_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('education_people', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('person_id');
+            $table->string('shcool_name', 100);
+            $table->unsignedBigInteger('status_id');
+            $table->unsignedBigInteger('studyLevel_id');
+            $table->foreign('person_id')->references('id')->on('users');
+            $table->foreign('status_id')->references('id')->on('study_statuses');
+            $table->foreign('studyLevel_id')->references('id')->on('study_levels');
             $table->timestamps();
         });
     }
